3ecd3f3986dd3855848fb5d97d3bc0f7bd017089
R Shiva Krishna <85845391+shivakrishnazikardh@users.noreply.github.com>
2025-07-31 23:09:49 -0500

    File updated at 7/31/2025, 11:09:51 PM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,4 +1,4 @@
-# Last updated: 7/31/2025, 11:07:22 PM
+# Last updated: 7/31/2025, 11:09:51 PM
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         trip = []
@@ -12,13 +12,10 @@ class Solution:
                 threeS = j + nums[l] + nums[r]
                 if threeS > 0 :
                     r -= 1
-                    while nums[r] == nums[r+1] and l<r and r<len(nums):
-                        r -= 1
+                   
                 elif threeS < 0 :
                     l += 1
-                    while nums[l] == nums[l-1] and l<r:
-                        l += 1
-
+                    
                 else :
                     trip.append([j, nums[l], nums[r]])
                     l += 1
