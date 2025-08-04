0a2a69d4478f5a011ab2de41405bf20759dd7761
R Shiva Krishna <85845391+shivakrishnazikardh@users.noreply.github.com>
2025-08-04 09:50:25 -0500

    File updated at 8/4/2025, 9:50:25 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,14 +1,13 @@
-# Last updated: 8/4/2025, 9:47:13 AM
+# Last updated: 8/4/2025, 9:50:25 AM
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        buy, sell = 0, 1
-        prof = 0
+        buy = 0
+        sell = 0
 
-        while sell < len(prices):
-            if prices[buy] < prices[sell] :
-                m = prices[sell] - prices[buy]
-                prof = max(m, prof)
-            else :
-                buy = sell
-            sell += 1
-        return prof
+        for i in range(len(prices)) :
+            if prices[i] < buy or i == 0:
+                buy = prices[i]
+            if (prices[i] - buy) > sell :
+                sell = prices[i] - buy
+
+        return sell
