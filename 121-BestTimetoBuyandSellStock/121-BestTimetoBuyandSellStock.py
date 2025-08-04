e79d9fbecd81ac7ac181dc8fa36ca03aa21479ea
R Shiva Krishna <85845391+shivakrishnazikardh@users.noreply.github.com>
2025-08-04 09:21:58 -0500

    Create 121-BestTimetoBuyandSellStock

---
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -0,0 +1,13 @@
+# Last updated: 8/4/2025, 9:21:59 AM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = 0
+        sell = 0
+
+        for i in range(len(prices)) :
+            if prices[i] < buy or i == 0:
+                buy = prices[i]
+            if (prices[i] - buy) > sell :
+                sell = prices[i] - buy
+
+        return sell
