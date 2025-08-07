7dd5483c745e150acef6491ab967b09522f0a417
R Shiva Krishna <85845391+shivakrishnazikardh@users.noreply.github.com>
2025-08-06 22:03:34 -0500

    File updated at 8/6/2025, 10:03:35 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -1,24 +1,24 @@
-# Last updated: 8/5/2025, 10:43:56 PM
+# Last updated: 8/6/2025, 10:03:35 PM
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        hash = []
+        hash = {}
         l, r = 0, 0
         charl = [0] * 26
         maxl, t = 0, 0
 
         for i in range(len(s)):
-            charl[ord(s[r]) - ord('A')] += 1
+            hash[s[r]] = hash.get(s[r], 0) + 1
             lenl = r - l + 1
 
-            if lenl - max(charl) > k :
-                charl[ord(s[l]) - ord('A')] -= 1
+            if lenl - max(hash.values()) > k :
+                hash[s[l]] -= 1
                 l += 1
                 t = 0
             
             else :
                 t += 1
                 maxl = max(maxl, lenl)
-                hash.append(maxl)
+                
             
             r += 1
 
