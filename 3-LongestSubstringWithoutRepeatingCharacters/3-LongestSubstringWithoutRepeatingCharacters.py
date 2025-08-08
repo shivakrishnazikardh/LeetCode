9ce9b84947e9ca637578c5ab4fb639d55be0cabf
R Shiva Krishna <85845391+shivakrishnazikardh@users.noreply.github.com>
2025-08-07 22:57:50 -0500

    File updated at 8/7/2025, 10:57:50 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,28 +1,19 @@
-# Last updated: 8/7/2025, 10:24:24 PM
-class MinStack:
+# Last updated: 8/7/2025, 10:57:50 PM
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        answer = [0]*len(temperatures)
 
-    def __init__(self):
-        self.stack = []
-        self.min = []
+        for i in range(len(temperatures)):
 
-    def push(self, val: int) -> None:
-        self.stack.append(val)
-        self.min.append(min(val, self.min[-1] if self.min else val))
+            
 
-    def pop(self) -> None:
-        self.stack.pop()
-        self.min.pop()
+            while stack and temperatures[stack[-1]] < temperatures[i]:
+                answer[stack[-1]] = (i - stack[-1])
+                stack.pop()
+            
 
-    def top(self) -> int:
-        return self.stack[-1]
+            stack.append(i)
 
-    def getMin(self) -> int:
-        return self.min[-1]
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        return answer
+        
