c1256dd064e8057d6a20ca14bc3dcfd4876f3937
R Shiva Krishna <85845391+shivakrishnazikardh@users.noreply.github.com>
2025-08-07 22:20:34 -0500

    File updated at 8/7/2025, 10:20:35 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,20 +1,27 @@
-# Last updated: 8/4/2025, 4:57:53 PM
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        hash = set()
-        l, r = 0, 0
-        msub= 0
+# Last updated: 8/7/2025, 10:20:35 PM
+class MinStack:
 
-        for i in range(len(s)):
-            while s[r] in hash :
-                hash.remove(s[l])
-                l += 1
-                                
-            
-            hash.add(s[r])
-            r += 1
-            msub = max(r-l, msub)
-                
-        return msub
-                
+    def __init__(self):
+        self.stack = []
+        self.min = []
 
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
